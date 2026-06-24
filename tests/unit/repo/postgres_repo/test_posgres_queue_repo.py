3002cf1f2db4bad1595ac7dc7c868259f3f8eda1
Nattapong-top <37808442+Nattapong-top@users.noreply.github.com>
2026-06-24 22:43:35 +0700

    feat(repo): implement update for PostgresQueueRepo (#72)

--- tests/unit/repo/postgres_repo/test_posgres_queue_repo.py
+++ tests/unit/repo/postgres_repo/test_posgres_queue_repo.py
@@ -10,3 +10,25 @@ def test_pg_queue_repo_should_save_and_retrieve_queue(pg_queue_table, new_queue)
     assert retrieve_queue is not None
     assert new_queue.id == retrieve_queue.id
     assert new_queue.patient_id == retrieve_queue.patient_id
+
+
+def test_pg_queue_repo_should_update_queue(pg_queue_table, queue, diagnosis):
+    repo = PostgresQueueRepository(pg_queue_table)
+    repo.save(queue)
+
+    queue.diagnosis = diagnosis
+    queue.status = queue.status.COMPLETED
+    queue.version = queue.version.increment()
+    repo.update(queue)
+
+    updated_queue = repo.get_by_queue_id(queue.id)
+
+    assert updated_queue is not None
+    assert queue.id == updated_queue.id
+    assert updated_queue.status.value == "ตรวจเสร็จแล้ว"
+    assert updated_queue.version.number == 2
+    assert updated_queue.diagnosis is not None
+
+    assert updated_queue.diagnosis.disease == "ไข้หวัดใหญ่"
+    assert len(updated_queue.diagnosis.medicine_prescribed) == 1
+    assert updated_queue.diagnosis.medicine_prescribed[0].name == "Paracetamol"
